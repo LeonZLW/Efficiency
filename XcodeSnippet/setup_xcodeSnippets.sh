4e0c0f7a08ae25c6f1db62c6d6d1d26f0d00c351
leon <leon_lw@126.com>
2017-08-10 02:25:50 +0800

    update

--- XcodeSnippet/setup_xcodeSnippets.sh
+++ XcodeSnippet/setup_xcodeSnippets.sh
@@ -3,7 +3,6 @@ mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/Use
 
 #rm ~/Library/Developer/Xcode/UserData/CodeSnippets
 
-SRC_XCODE_SNIPPET_HOME='pwd'
+SRC_XCODE_SNIPPET_HOME=`pwd`
 ln -s ${SRC_XCODE_SNIPPET_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-
 echo "Leon_Snippet_update_Done"
